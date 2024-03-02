05ecc504944ffa5e4ee0b21f01b33efcd5f14140
Ivan Semikin <i.semikin.94@icloud.com>
2024-03-02 17:23:19 +0100

    Initial Commit

---
+++ MyCalories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalories
+//
+//  Created by Иван Семикин on 02/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
