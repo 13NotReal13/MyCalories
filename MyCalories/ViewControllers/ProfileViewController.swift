0369d6359c455413c3f3ef0dfaf7fc7499cb6455
Ivan Semikin <i.semikin.94@icloud.com>
2024-04-01 18:43:16 +0200

    Setup UI in ProfileVC

---
+++ MyCalories/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  MyCalories
+//
+//  Created by Иван Семикин on 01/04/2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
