382f5960bc94c13f13e879fd361b75a42cffe4f7
Ivan Semikin <i.semikin.94@icloud.com>
2024-04-08 18:45:52 +0200

    Create model ProductsList

--- MyCalories/Models/ProductsList.swift
+++ MyCalories/Models/ProductsList.swift
@@ -1,8 +1,21 @@
 //
-//  ProductList.swift
+//  ProductsList.swift
 //  MyCalories
 //
 //  Created by Иван Семикин on 08/04/2024.
 //
 
 import Foundation
+import RealmSwift
+
+final class ProductsList: Object {
+    @Persisted var products = List<Product>()
+}
+
+final class Product: Object {
+    @Persisted var title = ""
+    @Persisted var protein = 0.0
+    @Persisted var fats = 0.0
+    @Persisted var carbohydrates = 0.0
+    @Persisted var calories = 0.0
+}
