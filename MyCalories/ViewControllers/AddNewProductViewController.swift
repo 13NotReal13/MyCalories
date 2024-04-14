f4444c23450870e5c0b1788033386cebe947782e
Ivan Semikin <i.semikin.94@icloud.com>
2024-04-14 18:30:49 +0200

    Create IBOutlets + IBActions in AddNewProductsVC

--- MyCalories/ViewControllers/AddNewProductViewController.swift
+++ MyCalories/ViewControllers/AddNewProductViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 final class AddNewProductViewController: UIViewController {
     
+    @IBOutlet var addBarButtonItem: UIBarButtonItem!
+    
+    @IBOutlet var nameTF: UITextField!
+    @IBOutlet var proteinTF: UITextField!
+    @IBOutlet var fatsTF: UITextField!
+    @IBOutlet var carbohydratesTF: UITextField!
+    @IBOutlet var caloriesTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    @IBAction func addBarButtonItemAction(_ sender: UIBarButtonItem) {
     }
 }
