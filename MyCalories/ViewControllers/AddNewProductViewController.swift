7989e9e55fd477786fab9b885aa902650873f6b7
Ivan Semikin <i.semikin.94@icloud.com>
2024-04-14 18:26:55 +0200

    Create AddNewProductVC with UI

---
+++ MyCalories/ViewControllers/AddNewProductViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddNewProductViewController.swift
+//  MyCalories
+//
+//  Created by Иван Семикин on 14/04/2024.
+//
+
+import UIKit
+
+final class AddNewProductViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
