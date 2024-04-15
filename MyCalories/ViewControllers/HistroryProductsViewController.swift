e1a3a3060950ed9e4c8ab16279c84dd96f0cb67c
Ivan Semikin <i.semikin.94@icloud.com>
2024-04-15 14:16:53 +0200

    Create HistroryProductsViewController with UI

---
+++ MyCalories/ViewControllers/HistroryProductsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HistroryProductsViewController.swift
+//  MyCalories
+//
+//  Created by Иван Семикин on 15/04/2024.
+//
+
+import UIKit
+
+final class HistroryProductsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
